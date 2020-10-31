83bd5726ecac0a8df3f43638d360a2263f713e5f
Nidhi Kumari <nidhi1691996@outlook.com>
2020-10-31 22:05:05 +0530

    removed analysis

--- src/components/SupremeCourt/index.js
+++ src/components/SupremeCourt/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 
 import MovementsSupremeCourtChart from './Charts/Movement';
 import StateCards from './States/cards.js';
-import Defination from './Analysis';
 
 class JusticeCourt extends Component {
     render() {
@@ -10,7 +9,6 @@ class JusticeCourt extends Component {
             <div className="part" style={{ marginTop: '40px' }}>
                 <div className="body-header"> Supreme Court Data </div>
                 <MovementsSupremeCourtChart />
-                <Defination />
                 <StateCards />
             </div>
         );
